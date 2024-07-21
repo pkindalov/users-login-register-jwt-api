b9150993df203f29d2142e39f1ad13c6e7ed72cc
pkindalov <panayotkindalov@gmail.com>
2024-07-21 13:14:11 +0300

    - Added new endpoint for updating user profile - /api/v1/profile

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,21 @@
+module Api
+  module V1
+    class UsersController < ApplicationController
+      before_action :authenticate_request!
+
+      def update
+        if @current_user.update(user_params)
+          render json: { message: 'Profile updated successfully' }, status: :ok
+        else
+          render json: { errors: @current_user.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def user_params
+        params.require(:user).permit(:email, :password)
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       post 'register', to: 'auth#register'
       post 'login', to: 'auth#login'
+      put 'profile', to: 'users#update'
     end
   end
 end
