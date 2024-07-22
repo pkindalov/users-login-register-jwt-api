1d0b532ec0ead41b8f22c2d7323788c2dca6ecad
pkindalov <panayotkindalov@gmail.com>
2024-07-22 09:01:36 +0300

    - Now user can upload avatar photo
    Example of using
    
    curl -X PUT "http://localhost:3000/api/v1/profile" -H "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo2fQ.881507vPTLLJVJSAteMvVNJ_wiTMiAebAo9uFzN0eAM" -F "user[email]=test@abv.bg" -F "user[password]=123456" -F "user[avatar]=@\"C:/Users/User/Documents/shutterstock/stock-photo-serene-night-camping-scene-with-a-glowing-full-moon-and-distant-hot-air-balloons-a-tranquil-escape-2318956105.jpg\""

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,10 +2,17 @@ module Api
   module V1
     class UsersController < ApplicationController
       before_action :authenticate_request!
+      include Rails.application.routes.url_helpers
 
       def update
         if @current_user.update(user_params)
-          render json: { message: 'Profile updated successfully' }, status: :ok
+          if params[:user][:avatar]
+            @current_user.avatar.attach(params[:user][:avatar])
+          end
+          render json: {
+            message: 'Profile updated successfully',
+            user: user_response(@current_user)
+          }, status: :ok
         else
           render json: { errors: @current_user.errors.full_messages }, status: :unprocessable_entity
         end
@@ -19,11 +26,22 @@ module Api
         end
       end
 
-
       private
 
       def user_params
-        params.require(:user).permit(:email, :password)
+        params.require(:user).permit(:email, :password, :avatar)
+      end
+
+      def user_response(user)
+        {
+          id: user.id,
+          email: user.email,
+          avatar_url: avatar_url(user)
+        }
+      end
+
+      def avatar_url(user)
+        user.avatar.attached? ? url_for(user.avatar) : nil
       end
     end
   end
--- app/middleware/authentication_middleware.rb
+++ app/middleware/authentication_middleware.rb
@@ -9,24 +9,28 @@ class AuthenticationMiddleware
   def call(env)
     request = Rack::Request.new(env)
 
-    if excluded_path?(request.env['REQUEST_PATH'])
-      return @app.call(env)
-    end
-
-    token = extract_token(request)
-
-    if valid_token?(token)
+    if excluded_path?(request.path)
       @app.call(env)
     else
-      [401, { 'Content-Type' => 'application/json' }, [{ error: 'Unauthorized' }.to_json]]
+      token = extract_token(request)
+
+      if valid_token?(token)
+        @app.call(env)
+      else
+        [401, { 'Content-Type' => 'application/json' }, [{ error: 'Unauthorized' }.to_json]]
+      end
     end
   end
 
   private
 
   def excluded_path?(path)
-    excluded_paths = %w[/api/v1/login /api/v1/register]
-    excluded_paths.include?(path)
+    excluded_paths = %w[
+      /api/v1/login
+      /api/v1/register
+      /rails/active_storage/
+    ]
+    excluded_paths.any? { |excluded_path| path.start_with?(excluded_path) }
   end
 
   def extract_token(request)
