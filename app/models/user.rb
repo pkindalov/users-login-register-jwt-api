3263bd897839b66e3a650a2eabaf36b8b039c58c
pkindalov <panayotkindalov@gmail.com>
2024-07-22 13:23:43 +0300

    - Added small validations on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   has_secure_password
   has_one_attached :avatar, dependent: :destroy
 
-  validates :email, presence: true, uniqueness: true
+  EMAIL_REGEX = %r{\A(?=[a-z0-9@.!#$%&'*+\-/=?^_`{|}~]{6,254}\z)(?=[a-z0-9.!#$%&'*+\-/=?^_`{|}~]{1,64}@)[a-z0-9!#$%&'*+\-/=?^_`{|}~]+(?:\.[a-z0-9!#$%&'*+\-/=?^_`{|}~]+)*@(?:(?=[a-z0-9\-]{1,63}\.)[a-z0-9](?:[a-z0-9\-]*[a-z0-9])?\.)+(?=[a-z0-9\-]{1,63}\z)[a-z0-9](?:[a-z0-9\-]*[a-z0-9])?\z}i
+  validates :email, presence: true, uniqueness: true, format: { with: EMAIL_REGEX }
+  validates :password, presence: true, length: { minimum: 6 }, if: :password
 end
